aa70da6dcd234841db89c161b1c28f00778d461e
Ganesh1493 <136460259+Ganesh1493@users.noreply.github.com>
2023-08-19 15:34:29 +0530

    Update vcube.tf

--- vcube.tf
+++ vcube.tf
@@ -5,20 +5,20 @@ provider "aws" {
 }
 
 # Creating a bucket
-  resource "aws_s3_bucket" "web" {
+  /*resource "aws_s3_bucket" "web" {
       bucket = "s3bucketviaterraform"
       acl    = "private"   # or can be "public-read"
   tags = {
     Name        = "My bucket"
     Environment = "Dev" 
-  }  
+  }  */
 
 #creating an ec2 instance
-/*  resource "aws_instance" "web" {
+  resource "aws_instance" "web" {
   ami           = "ami-024e6efaf93d85776"
   instance_type = "t2.micro"
   key_name      = "KeyPair4alltools"
   tags = {
     Name = "ec2viaterraform"
-  } */
+  } 
 }
